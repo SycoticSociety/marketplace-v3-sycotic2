ffa30ba9713060ef8c9c9887ab3a6f4f663c8270
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-03-10 01:32:36 -0500

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -3,16 +3,15 @@
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+export const NETWORK = Cronos;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
+export const MARKETPLACE_ADDRESS = "0xE666c5B5964aA789524b03392f02904319adf2da";
 
 // 3. The address of your NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+export const NFT_COLLECTION_ADDRESS = "0x2111ac0D4178274F8f859401a16dB9fBB524986E";
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://cronoscan.com/";
